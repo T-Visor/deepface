004ceaace9870fe73583b8542141241846a95635
T-Visor <turkimbro@yahoo.com>
2022-04-08 19:38:59 -0400

    updated requirements

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # For reference: https://pypi.org/project/deepface/
 from PIL import Image
 from numpy import asarray
-#from deepface import DeepFace
+from deepface import DeepFace
 from matplotlib import pyplot
 
 backends = ['opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface', 'mediapipe']
@@ -16,15 +16,15 @@ second_image = 'lowkey_image.jpg'
 #        detector_backend = backends[3])
 #print(df.to_string())
 
-#result = DeepFace.verify(img1_path=first_image, img2_path=second_image, detector_backend=backends[3])
-#print(result)
+result = DeepFace.verify(img1_path=first_image, img2_path=second_image, detector_backend=backends[3])
+print(result)
 
 img = Image.open(first_image) 
 img2 = Image.open(second_image) 
 
 i = 1
 for image in [img, img2]:
-    pyplot.suptitle('Images Provided', fontsize=20)
+    pyplot.suptitle('Images provided', fontsize=20)
     pyplot.subplot(1, 2, i) # 1-row, 2 columns
     pyplot.axis('off')
     pyplot.imshow(image)
