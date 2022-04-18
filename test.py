4bb537691fc4b18f89ba9e39066e2f60b0891028
T-Visor <turkimbro@yahoo.com>
2022-04-18 15:56:11 -0400

    made progress on deepface work

--- test.py
+++ test.py
@@ -3,30 +3,58 @@ from PIL import Image
 from numpy import asarray
 from deepface import DeepFace
 from matplotlib import pyplot
+import sys
+import argparse
 
-backends = ['opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface', 'mediapipe']
-models = ["VGG-Face", "Facenet", "Facenet512", "OpenFace", "DeepFace", "DeepID", "ArcFace", "Dlib"]
 
-first_image = 'angelina.jpg'
-second_image = 'lowkey_image.jpg'
 
-#df = DeepFace.find(img_path =
-#        "../sample-face-recognition/5-celebrity-faces-dataset/val/elton_john/httpafilesbiographycomimageuploadcfillcssrgbdprgfacehqwMTEODAOTcxNjcMjczMjkzjpg.jpg",
-#        db_path = "../sample-face-recognition/5-celebrity-faces-dataset/train",
-#        detector_backend = backends[3])
-#print(df.to_string())
 
-result = DeepFace.verify(img1_path=first_image, img2_path=second_image, detector_backend=backends[3])
-print(result)
+def main():
+    # Optional arguments for DeepFace.
+    backends = ['opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface', 'mediapipe']
+    models = ["VGG-Face", "Facenet", "Facenet512", "OpenFace", "DeepFace", "DeepID", "ArcFace", "Dlib"]
 
-img = Image.open(first_image) 
-img2 = Image.open(second_image) 
+    arguments = parse_command_line_arguments()
+    source_image = arguments.source_image[0]
+    database_path = arguments.database_path[0]
 
-i = 1
-for image in [img, img2]:
-    pyplot.suptitle('Images provided', fontsize=20)
-    pyplot.subplot(1, 2, i) # 1-row, 2 columns
-    pyplot.axis('off')
-    pyplot.imshow(image)
-    i += 1
-pyplot.show()
+    print(source_image)
+
+
+    #df = DeepFace.find(img_path = "",
+    #                   db_path = "",
+    #                   detector_backend = backends[3])
+    #print(df.to_string())
+
+
+
+
+def parse_command_line_arguments():
+    """ 
+        Parse the arguments from the command-line.
+        If no arguments are passed, the help screen will
+        be shown and the program will be terminated.
+
+    Returns:
+        the parser with command-line arguments
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-s', '--source_image', nargs=1, required=True, 
+                        help='Image of person to identify.')
+
+    parser.add_argument('-d', '--database_path', nargs=1, required=True, 
+                        help='Dataset of images containing faces.')
+
+    # if no arguments were passed, show the help screen
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit()
+
+    return parser.parse_args()
+
+
+
+
+if __name__ == '__main__':
+    main()
