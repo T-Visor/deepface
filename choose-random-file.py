f8fcee34d74f68b2df32c33662fa55bbcb868cd5
T-Visor <turkimbro@yahoo.com>
2022-05-03 13:12:02 -0400

    choosing random files from each class has been implemented, need to integrate into main framework

--- choose-random-file.py
+++ choose-random-file.py
@@ -5,15 +5,13 @@ import random
 
 # Reference: https://stackoverflow.com/questions/71873989/how-to-select-single-random-file-from-each-sub-directory-in-python
 source_directory = './images/original-images/'
-parent_folders = None
 selected_images = []
 
 # Select a random file from each subdirectory and store the subdirectories
 for root, directories, files in os.walk(source_directory):
-    if directories:
-        parent_folders = directories
-    if not files: continue
-    selected_images.append(random.choice(files))
+    if not files:
+        continue
+    selected_images.append(str(root) + '/' + random.choice(files))
 
 for image in selected_images:
     print(image)
